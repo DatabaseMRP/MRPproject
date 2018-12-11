85d78bb99044da67e3a491dfcc16bdb7251c464f
Luke Steffen <lhs3@calvin.edu>
2018-12-10 21:03:56 -0500

    UI shell

---
+++ MRPCustomer/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace MRPCustomer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.createCustomer = new System.Windows.Forms.Button();
+            this.createOrder = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.createCustomer, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.createOrder, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.textBox1, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(239, 190);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // createCustomer
+            // 
+            this.createCustomer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.createCustomer.Location = new System.Drawing.Point(3, 129);
+            this.createCustomer.Name = "createCustomer";
+            this.createCustomer.Size = new System.Drawing.Size(113, 58);
+            this.createCustomer.TabIndex = 1;
+            this.createCustomer.Text = "Create/Find Customer Account";
+            this.createCustomer.UseVisualStyleBackColor = true;
+            this.createCustomer.Click += new System.EventHandler(this.createCustomer_Click);
+            // 
+            // createOrder
+            // 
+            this.createOrder.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.createOrder.Location = new System.Drawing.Point(122, 129);
+            this.createOrder.Name = "createOrder";
+            this.createOrder.Size = new System.Drawing.Size(114, 58);
+            this.createOrder.TabIndex = 2;
+            this.createOrder.Text = "Create Customer Order";
+            this.createOrder.UseVisualStyleBackColor = true;
+            this.createOrder.Click += new System.EventHandler(this.createOrder_Click);
+            // 
+            // textBox1
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.textBox1, 2);
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox1.Location = new System.Drawing.Point(3, 3);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(233, 89);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Text = "Welcome to the MRP customer manager. Select an option to begin.";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(239, 190);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "MRP Customer";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button createCustomer;
+        private System.Windows.Forms.Button createOrder;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
