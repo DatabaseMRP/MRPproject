0837fdc530d5b81af8b67ab1a14687ad4e2b7913
Luke Steffen <lhs3@calvin.edu>
2018-12-12 22:14:13 -0500

    Create order shell

--- MRPCustomer/CustomerOrder.Designer.cs
+++ MRPCustomer/CustomerOrder.Designer.cs
@@ -30,44 +30,283 @@
         {
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.priceButton = new System.Windows.Forms.Button();
+            this.customerNameBox = new System.Windows.Forms.TextBox();
+            this.productNameBox = new System.Windows.Forms.TextBox();
+            this.customerIDBox = new System.Windows.Forms.TextBox();
+            this.ProductIDsBox = new System.Windows.Forms.TextBox();
+            this.quantityBox = new System.Windows.Forms.TextBox();
+            this.customerButton = new System.Windows.Forms.Button();
+            this.productButton = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.orderButton = new System.Windows.Forms.Button();
+            this.customerResult = new System.Windows.Forms.Label();
+            this.productsResult = new System.Windows.Forms.Label();
+            this.priceResult = new System.Windows.Forms.Label();
             this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // tableLayoutPanel1
             // 
-            this.tableLayoutPanel1.ColumnCount = 3;
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 80F));
-            this.tableLayoutPanel1.Controls.Add(this.button1, 2, 0);
+            this.tableLayoutPanel1.ColumnCount = 4;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 15F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 15F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 15F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 55F));
+            this.tableLayoutPanel1.Controls.Add(this.button1, 3, 8);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label3, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.label4, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.label5, 0, 4);
+            this.tableLayoutPanel1.Controls.Add(this.checkBox1, 0, 5);
+            this.tableLayoutPanel1.Controls.Add(this.priceButton, 1, 5);
+            this.tableLayoutPanel1.Controls.Add(this.customerNameBox, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.productNameBox, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.customerIDBox, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.ProductIDsBox, 1, 3);
+            this.tableLayoutPanel1.Controls.Add(this.quantityBox, 1, 4);
+            this.tableLayoutPanel1.Controls.Add(this.customerButton, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.productButton, 2, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label6, 2, 3);
+            this.tableLayoutPanel1.Controls.Add(this.orderButton, 0, 7);
+            this.tableLayoutPanel1.Controls.Add(this.customerResult, 3, 0);
+            this.tableLayoutPanel1.Controls.Add(this.productsResult, 3, 1);
+            this.tableLayoutPanel1.Controls.Add(this.priceResult, 2, 5);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-            this.tableLayoutPanel1.RowCount = 2;
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowCount = 9;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 8F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 8F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 8F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 8F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 8F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 8F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 8F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 8F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 36F));
             this.tableLayoutPanel1.Size = new System.Drawing.Size(800, 450);
             this.tableLayoutPanel1.TabIndex = 0;
-            
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(163, 3);
+            this.button1.Location = new System.Drawing.Point(363, 291);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(145, 46);
             this.button1.TabIndex = 0;
             this.button1.Text = "connect to database";
             this.button1.UseVisualStyleBackColor = true;
             // 
-            // Form2
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.label1.Location = new System.Drawing.Point(15, 23);
+            this.label1.Margin = new System.Windows.Forms.Padding(15, 15, 3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(102, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Customer Name: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.label2.Location = new System.Drawing.Point(15, 59);
+            this.label2.Margin = new System.Windows.Forms.Padding(15, 15, 3, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(102, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Product Name: ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.label3.Location = new System.Drawing.Point(15, 95);
+            this.label3.Margin = new System.Windows.Forms.Padding(15, 0, 3, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(102, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Customer ID: ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.label4.Location = new System.Drawing.Point(15, 131);
+            this.label4.Margin = new System.Windows.Forms.Padding(15, 0, 3, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(102, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Product ID(s): ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.label5.Location = new System.Drawing.Point(15, 167);
+            this.label5.Margin = new System.Windows.Forms.Padding(15, 0, 3, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(102, 13);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Product Quantity(s): ";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.checkBox1.Location = new System.Drawing.Point(15, 196);
+            this.checkBox1.Margin = new System.Windows.Forms.Padding(15, 3, 3, 3);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(102, 17);
+            this.checkBox1.TabIndex = 6;
+            this.checkBox1.Text = "One shipment";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // priceButton
+            // 
+            this.priceButton.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.priceButton.Location = new System.Drawing.Point(123, 190);
+            this.priceButton.Name = "priceButton";
+            this.priceButton.Size = new System.Drawing.Size(114, 23);
+            this.priceButton.TabIndex = 7;
+            this.priceButton.Text = "Calculate price";
+            this.priceButton.UseVisualStyleBackColor = true;
+            this.priceButton.Click += new System.EventHandler(this.priceButton_Click);
+            // 
+            // customerNameBox
+            // 
+            this.customerNameBox.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.customerNameBox.Location = new System.Drawing.Point(123, 13);
+            this.customerNameBox.Name = "customerNameBox";
+            this.customerNameBox.Size = new System.Drawing.Size(114, 20);
+            this.customerNameBox.TabIndex = 8;
+            // 
+            // productNameBox
+            // 
+            this.productNameBox.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.productNameBox.Location = new System.Drawing.Point(123, 49);
+            this.productNameBox.Name = "productNameBox";
+            this.productNameBox.Size = new System.Drawing.Size(114, 20);
+            this.productNameBox.TabIndex = 9;
+            // 
+            // customerIDBox
+            // 
+            this.customerIDBox.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.customerIDBox.Location = new System.Drawing.Point(123, 85);
+            this.customerIDBox.Name = "customerIDBox";
+            this.customerIDBox.Size = new System.Drawing.Size(114, 20);
+            this.customerIDBox.TabIndex = 10;
+            // 
+            // ProductIDsBox
+            // 
+            this.ProductIDsBox.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.ProductIDsBox.Location = new System.Drawing.Point(123, 121);
+            this.ProductIDsBox.Name = "ProductIDsBox";
+            this.ProductIDsBox.Size = new System.Drawing.Size(114, 20);
+            this.ProductIDsBox.TabIndex = 11;
+            // 
+            // quantityBox
+            // 
+            this.quantityBox.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.quantityBox.Location = new System.Drawing.Point(123, 157);
+            this.quantityBox.Name = "quantityBox";
+            this.quantityBox.Size = new System.Drawing.Size(114, 20);
+            this.quantityBox.TabIndex = 12;
+            // 
+            // customerButton
+            // 
+            this.customerButton.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.customerButton.Location = new System.Drawing.Point(243, 10);
+            this.customerButton.Name = "customerButton";
+            this.customerButton.Size = new System.Drawing.Size(114, 23);
+            this.customerButton.TabIndex = 13;
+            this.customerButton.Text = "Search Customer";
+            this.customerButton.UseVisualStyleBackColor = true;
+            this.customerButton.Click += new System.EventHandler(this.customerButton_Click);
+            // 
+            // productButton
+            // 
+            this.productButton.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.productButton.Location = new System.Drawing.Point(243, 46);
+            this.productButton.Name = "productButton";
+            this.productButton.Size = new System.Drawing.Size(114, 23);
+            this.productButton.TabIndex = 14;
+            this.productButton.Text = "Seach Products";
+            this.productButton.UseVisualStyleBackColor = true;
+            this.productButton.Click += new System.EventHandler(this.productButton_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.tableLayoutPanel1.SetColumnSpan(this.label6, 2);
+            this.label6.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.label6.Location = new System.Drawing.Point(255, 131);
+            this.label6.Margin = new System.Windows.Forms.Padding(15, 0, 3, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(542, 13);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Please separate the product IDs and quantities with a comma. Example: 1,2,3";
+            // 
+            // orderButton
+            // 
+            this.orderButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.orderButton.Location = new System.Drawing.Point(3, 255);
+            this.orderButton.Name = "orderButton";
+            this.orderButton.Size = new System.Drawing.Size(114, 30);
+            this.orderButton.TabIndex = 16;
+            this.orderButton.Text = "Create Order";
+            this.orderButton.UseVisualStyleBackColor = true;
+            this.orderButton.Click += new System.EventHandler(this.orderButton_Click);
+            // 
+            // customerResult
+            // 
+            this.customerResult.AutoSize = true;
+            this.customerResult.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.customerResult.Location = new System.Drawing.Point(363, 3);
+            this.customerResult.Margin = new System.Windows.Forms.Padding(3, 3, 3, 0);
+            this.customerResult.Name = "customerResult";
+            this.customerResult.Size = new System.Drawing.Size(434, 33);
+            this.customerResult.TabIndex = 17;
+            // 
+            // productsResult
+            // 
+            this.productsResult.AutoSize = true;
+            this.productsResult.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.productsResult.Location = new System.Drawing.Point(363, 39);
+            this.productsResult.Margin = new System.Windows.Forms.Padding(3, 3, 3, 0);
+            this.productsResult.Name = "productsResult";
+            this.productsResult.Size = new System.Drawing.Size(434, 33);
+            this.productsResult.TabIndex = 18;
+            // 
+            // priceResult
+            // 
+            this.priceResult.AutoSize = true;
+            this.priceResult.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.priceResult.Location = new System.Drawing.Point(243, 203);
+            this.priceResult.Name = "priceResult";
+            this.priceResult.Size = new System.Drawing.Size(114, 13);
+            this.priceResult.TabIndex = 19;
+            // 
+            // CustomerOrder
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.tableLayoutPanel1);
-            this.Name = "Form2";
+            this.Name = "CustomerOrder";
             this.Text = "Create Customer Account";
             this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -76,6 +315,25 @@
 
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button priceButton;
+        private System.Windows.Forms.TextBox customerNameBox;
+        private System.Windows.Forms.TextBox productNameBox;
+        private System.Windows.Forms.TextBox customerIDBox;
+        private System.Windows.Forms.TextBox ProductIDsBox;
+        private System.Windows.Forms.TextBox quantityBox;
+        private System.Windows.Forms.Button customerButton;
+        private System.Windows.Forms.Button productButton;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button orderButton;
+        private System.Windows.Forms.Label customerResult;
+        private System.Windows.Forms.Label productsResult;
+        private System.Windows.Forms.Label priceResult;
     }
 }
 
