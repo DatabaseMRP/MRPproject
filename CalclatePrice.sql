ebe071e4a03fa932b78e435e8a1b968287e47b96
kellysoh <43157961+kellysoh@users.noreply.github.com>
2018-12-12 23:19:38 -0500

    Update CalclatePrice.sql

--- CalclatePrice.sql
+++ CalclatePrice.sql
@@ -1,20 +1,25 @@
+IF OBJECT_ID('CalculatePrice', 'P') IS NOT NULL
+DROP PROCEDURE CalculatePrice; 
+GO 
+
+
+
 CREATE PROCEDURE CalculatePrice @InventoryID int, 
-@previousPrice DECIMAL(4,2), 
-@TOTALPRICE DECIMAL(4,2) OUTPUT 
+@previousPrice DECIMAL(10,2), 
+@quantity int, 
+@TOTALPRICE DECIMAL(10,2) OUTPUT 
 AS
 SET @TOTALPRICE = @previousPrice + 
 (SELECT BestPrice From Inventory
-WHERE InventoryID = @InventoryID);
+WHERE InventoryID = @InventoryID) * @quantity;
 
 RETURN @TOTALPRICE
 GO 
 
-/*example of usage 
+/*
 DECLARE @id int;
 SET @id = 2;
-DECLARE @Total decimal(4,2); 
-
-exec CalculatePrice 2, 0, @Total OUTPUT;
-
+DECLARE @Total decimal(10,2); 
+exec CalculatePrice 2, 0, 3, @Total OUTPUT;
 select @Total;
 */
